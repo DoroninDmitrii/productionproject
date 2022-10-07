f94448407abdcc94f8040bd93e6f636f59386874
Dmitrii Doronin <doronin_dima@mail.ru>
2022-10-07 15:11:52 +0300

    12 navbar ui kit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,9 @@
 import './styles/index.scss';
-import { Link } from 'react-router-dom';
 
 import { useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppRouter } from './providers/router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
 
@@ -11,10 +11,9 @@ const App = () => {
 
   return (
     <div className={classNames('app', {}, [theme])}>
+      <Navbar />   
+      <AppRouter />
       <button onClick={toggleTheme}>TOGGLE</button>
-      <Link to={'/'}>Main Page</Link>
-      <Link to={'/about'}>About Page</Link>
-      <AppRouter />   
     </div>
   );
 };
