b1ae1dd5eb4d8f6c764bd0519d2c913855428feb
Dmitrii Doronin <doronin_dima@mail.ru>
2022-10-16 16:39:52 +0300

    15 reactu18i finish

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,23 +6,6 @@ import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import { Suspense } from 'react';
-import { useTranslation } from 'react-i18next';
-
-const Component = () => {
-
-  const {t, i18n} = useTranslation();
-
-  const toggle = () => {
-    i18n.changeLanguage(i18n.language === 'en' ? 'ru' : 'en');
-  }
-
-  return (
-    <div>
-      <button onClick={toggle}>{t('Translate')}</button>
-      {t('Test translation')}
-    </div>
-  ) 
-}
 
 const App = () => {
 
@@ -32,7 +15,6 @@ const App = () => {
     <div className={classNames('app', {}, [theme])}>
       <Suspense fallback=''>
         <Navbar />
-          <Component/>
         <div className='content-page'>
           <Sidebar/>
           <AppRouter />
