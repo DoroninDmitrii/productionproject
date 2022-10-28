c9aaa44d60fbe0940440ef852c43be1887cadfd8
Dmitrii Doronin <doronin_dima@mail.ru>
2022-10-28 08:11:09 +0300

    23 - BundleAnalyzer

--- build/221.76b29a0293065e01b58d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkproductionproject=self.webpackChunkproductionproject||[]).push([[221],{221:(t,e,o)=>{o.r(e),o.d(e,{default:()=>c});var r=o(893),u=o(327);const c=function(){var t=(0,u.$)("about"),e=t.t;return t.i18n,(0,r.jsx)("div",{children:e("About page")},void 0)}}}]);
