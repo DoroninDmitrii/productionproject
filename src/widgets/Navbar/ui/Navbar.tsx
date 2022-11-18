f0ed4bf7d4b70dc56b50c72751760571821254f1
Dmitrii Doronin <doronin_dima@mail.ru>
2022-11-18 16:12:51 +0300

    add stories fix lint

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -28,7 +28,7 @@ export const Navbar = ({ className }: NavbarProps) => {
               {t('Log in')}
           </Button>
           <Modal isOpen={isAuthModal} isClose={onToggleModal}>
-              You want to write maintainable tests for your React components. As a part of this goal, you want your tests to avoid including implementation details of your components and rather focus on making your tests give you the confidence for which they are intended. As part of this, you want your testbase to be maintainable in the long run so refactors of your components (changes to implementation but not functionality) don't break your tests and slow you and your team down.
+              You want to write maintainable tests for your React components. As a part of this goal, you want your tests to avoid including implementation details of your components and rather focus on making your tests give you the confidence for which they are intended. As part of this, you want your testbase to be maintainable in the long run so refactors of your components (changes to implementation but not functionality) dont break your tests and slow you and your team down.
           </Modal>
       </div>
   )
