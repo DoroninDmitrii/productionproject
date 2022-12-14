30747e5afffc4fb55269263ea4ba3cfb3ed21dbd
Dmitrii Doronin <doronin_dima@mail.ru>
2022-12-14 19:36:39 +0300

    34 - create initAuthdata

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,10 +4,18 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import { AppRouter } from './providers/router'
 import { Navbar } from 'widgets/Navbar'
 import { Sidebar } from 'widgets/Sidebar'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { userAction } from 'entities/User'
 
 const App = () => {
   const { theme } = useTheme()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(userAction.initAuthData())
+  }, [dispatch])
+
   return (
       <div className={classNames('app', {}, [theme])}>
           <Suspense fallback=''>
