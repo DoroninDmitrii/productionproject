70df5aa40bab1bf09868c10c82febe33dc09213b
Dmitrii Doronin <doronin_dima@mail.ru>
2022-12-29 14:26:44 +0300

    38 add api axios

--- src/features/AuthByUsername/modal/services/loginByUserName/loginByUsername.ts
+++ src/features/AuthByUsername/modal/services/loginByUserName/loginByUsername.ts
@@ -10,21 +10,22 @@ interface LoginByUsernameProps {
 
 export const loginByUsername = createAsyncThunk<User, LoginByUsernameProps, { rejectValue: string }>(
   'users/loginByUsername',
-  async ({ password, username }, thunkAPI) => {
+  async ({ password, username }, { extra, dispatch, rejectWithValue }) => {
     try {
-      const response = await axios.post<User>('http://localhost:8000/login', { username, password })
+      // @ts-expect-error
+      const response = await extra.api.post<User>('/login', { username, password })
 
       if (!response.data) {
         throw new Error()
       }
 
       localStorage.setItem(USER_LOCALSTORAGE_KEY, JSON.stringify(response.data))
-      thunkAPI.dispatch(userAction.setAuthData(response.data))
+      dispatch(userAction.setAuthData(response.data))
 
       return response.data
     } catch (e) {
       console.log(e)
-      return thunkAPI.rejectWithValue('Error')
+      return rejectWithValue('Error')
     }
   }
 )
