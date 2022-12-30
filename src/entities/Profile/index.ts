759b2606cb15173265330ea3df48a3353a5bdc72
Dmitrii Doronin <doronin_dima@mail.ru>
2022-12-30 13:24:05 +0300

    39 add fetch profile

--- src/entities/Profile/index.ts
+++ src/entities/Profile/index.ts
@@ -1,5 +1,7 @@
 import { Profile, ProfileSchema } from './model/types/profile'
 import { profileAction, profileReducer } from './model/slice/profileSlice'
+import { fetchProfileData } from './model/services/fetchProfileData/fetchProfileData'
 
 export { Profile, ProfileSchema }
 export { profileAction, profileReducer }
+export { fetchProfileData }
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,7 +1,9 @@
-import { profileReducer } from 'entities/Profile'
+import { fetchProfileData, profileReducer } from 'entities/Profile'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { DynamicModuleLoader, ReducerList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch'
 
 const reducers: ReducerList = {
   profile: profileReducer
@@ -12,6 +14,11 @@ interface ProfilePageProps {
 
 const ProfilePage = ({ className }: ProfilePageProps) => {
   const { t, i18n } = useTranslation()
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProfileData())
+  }, [dispatch])
 
   return (
       <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
