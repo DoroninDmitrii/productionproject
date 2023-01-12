41b9d615116af65a95df46aeb42003257829c044
Dmitrii Doronin <doronin_dima@mail.ru>
2023-01-12 19:23:27 +0300

    43 add stories for profilecard

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { ProfileCard } from './ProfileCard'
+import { Country } from 'entities/Country'
+import { Currency } from 'entities/Currency'
+import avatar from 'shared/assets/test/ny-city.jpg'
+
+export default {
+  title: 'entities/ProfileCard',
+  component: ProfileCard,
+  argTypes: {
+    backgroundColor: { control: 'color' }
+  }
+} as ComponentMeta<typeof ProfileCard>
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  data: {
+    username: 'admin',
+    age: 30,
+    country: Country.Ukraine,
+    lastname: 'Ivanov',
+    first: 'Ivan',
+    city: 'Moscow',
+    currency: Currency.USD,
+    avatar
+  }
+}
+
+export const withError = Template.bind({})
+withError.args = {
+  error: 'true'
+}
+
+export const isLoading = Template.bind({})
+isLoading.args = {
+  isLoading: true
+}
+
+export const readonly = Template.bind({})
+readonly.args = {
+  readonly: true
+}
