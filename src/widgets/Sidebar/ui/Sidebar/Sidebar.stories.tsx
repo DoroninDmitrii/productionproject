b490e1452044833287a96a12e6f79ec4997a26b8
Dmitrii Doronin <doronin_dima@mail.ru>
2023-01-19 21:15:02 +0300

    43 slice tests and fix stories

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.tsx
@@ -4,6 +4,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Sidebar } from './Sidebar'
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import { Theme } from 'app/providers/ThemeProvider'
+import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator'
 
 export default {
   title: 'widgets/Sidebar',
@@ -18,7 +19,31 @@ const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
 export const Light = Template.bind({})
 Light.args = {}
 
+Light.decorators = [
+  ThemeDecorator(Theme.LIGHT),
+  StoreDecorator({
+    user: {
+      authData: {}
+    }
+  })
+]
+
 export const Dark = Template.bind({})
 Dark.args = {}
 
-Dark.decorators = [ThemeDecorator(Theme.DARK)]
+Dark.decorators = [
+  ThemeDecorator(Theme.DARK),
+  StoreDecorator({
+    user: {
+      authData: {}
+    }
+  })
+]
+
+export const NoAuth = Template.bind({})
+NoAuth.args = {}
+NoAuth.decorators = [
+  StoreDecorator({
+    user: {}
+  })
+]
