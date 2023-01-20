efc93d257f3a882f35689da7be7f806209b3cb4a
Dmitrii Doronin <doronin_dima@mail.ru>
2023-01-20 22:36:59 +0300

    46 add refresh plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,29 +2,14 @@ import webpack from 'webpack'
 import { BuildOptions } from './types/config'
 import { buildCssLoaders } from './loaders/buildCssLoaders'
 import { buildSvgLoader } from './loaders/buildSvgLoader'
+import { buildBabelLoader } from './loaders/buildBabelLoader'
 
-export function buildLoaders ({ isDev }: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders (options: BuildOptions): webpack.RuleSetRule[] {
+  const { isDev } = options
+
   const svgLoader = buildSvgLoader()
 
-  const babelLoader = {
-    test: /\.(js|jsx|tsx)$/,
-    exclude: /node_modules/,
-    use: {
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env'],
-        plugins: [
-          [
-            'i18next-extract',
-            {
-              locales: ['ru', 'en'],
-              keyAsDefaultValue: true
-            }
-          ]
-        ]
-      }
-    }
-  }
+  const babelLoader = buildBabelLoader(options)
 
   const cssLoader = buildCssLoaders(isDev)
 
