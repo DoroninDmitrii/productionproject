b79be696218c63926089a109f53d26314e3dc63b
Dmitrii Doronin <doronin_dima@mail.ru>
2023-02-03 20:58:10 +0300

    50 add image

--- src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
+++ src/entities/Article/ui/ArticleImageBlockComponent/ArticleImageBlockComponent.tsx
@@ -2,6 +2,7 @@ import { memo } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import { useTranslation } from 'react-i18next'
 import { ArticleImageBlock } from '../../model/types/article'
+import { Text, TextAlign } from 'shared/ui/Text/Text'
 import cls from './ArticleImageBlockComponent.module.scss'
 
 interface ArticleImageBlockComponentProps {
@@ -15,7 +16,10 @@ export const ArticleImageBlockComponent = memo((props: ArticleImageBlockComponen
 
   return (
       <div className={classNames(cls.ArticleImageBlockComponent, {}, [className])}>
-          {t('ArticleImageBlockComponent')}
+          <img src={block.src} alt={block.title} className={cls.img} />
+          { block.title && (
+          <Text text={block.title} align={TextAlign.CENTER}/>
+          )}
       </div>
   )
 })
