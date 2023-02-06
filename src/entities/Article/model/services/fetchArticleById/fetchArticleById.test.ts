ba6eff4838e60fefb9a10d5ad0272f0acbdef702
Dmitrii Doronin <doronin_dima@mail.ru>
2023-02-06 18:47:50 +0300

    50 fix test

--- src/entities/Article/model/services/fetchArticleById/fetchArticleById.test.ts
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.test.ts
@@ -1,15 +1,14 @@
 import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk'
 import { fetchArticlebyId } from './fetchArticleById'
 
-const data = {
-  id: '1'
-}
-
 describe('fetchArticlebyId.test', () => {
+  const data = {
+    id: '1'
+  }
   test('success', async () => {
     const thunk = new TestAsyncThunk(fetchArticlebyId)
     thunk.api.get.mockReturnValue(Promise.resolve({ data }))
-    const result = await thunk.callThunk(data)
+    const result = await thunk.callThunk('1')
 
     expect(thunk.api.get).toHaveBeenCalled()
     expect(result.meta.requestStatus).toBe('fulfilled')
@@ -19,7 +18,7 @@ describe('fetchArticlebyId.test', () => {
   test('error', async () => {
     const thunk = new TestAsyncThunk(fetchArticlebyId)
     thunk.api.get.mockReturnValue(Promise.resolve({ status: 403 }))
-    const result = await thunk.callThunk(data)
+    const result = await thunk.callThunk('1')
     expect(result.meta.requestStatus).toBe('rejected')
   })
 })
