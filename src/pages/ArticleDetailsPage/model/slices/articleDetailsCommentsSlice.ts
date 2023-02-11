368f2f28c3841fe18d59a1fa3f2b8cd63c77fd68
Dmitrii Doronin <doronin_dima@mail.ru>
2023-02-11 10:24:38 +0300

    51 slice with noraliz is ready

--- src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
@@ -4,19 +4,27 @@ import {
 } from '@reduxjs/toolkit'
 import { StateSchema } from 'app/providers/StoreProvider'
 import { Comment } from 'entities/Comment'
+import { ArticleDetailsCommentSchema } from '../types/ArticleDetailsCommentSchema'
 
 const commentsAdapter = createEntityAdapter<Comment>({
   selectId: (comment) => comment.id
 })
 
-const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
-  (state ) => state.articleDetailsComments || commentsAdapter.getInitialState()
+export const getArticleComments = commentsAdapter.getSelectors<StateSchema>(
+  (state) => state.articleDetailsComments || commentsAdapter.getInitialState()
 )
 
 const articleDetailsCommentsSlice = createSlice({
   name: 'articleDetailsCommentsSlice',
-  initialState: booksAdapter.getInitialState(),
+  initialState: commentsAdapter.getInitialState<ArticleDetailsCommentSchema>({
+    isLoading: false,
+    error: undefined,
+    ids: [],
+    entities: {}
+  }),
   reducers: {
 
   }
 })
+
+export const { reducer: articleDetailsCommnetsReducer } = articleDetailsCommentsSlice
--- src/pages/ArticleDetailsPage/model/types/ArticleDetailsCommentSchema.ts
+++ src/pages/ArticleDetailsPage/model/types/ArticleDetailsCommentSchema.ts
@@ -1,5 +1,7 @@
-export interface ArticleDetailsCommentSchema {
+import { EntityState } from '@reduxjs/toolkit'
+import { Comment } from 'entities/Comment'
+
+export interface ArticleDetailsCommentSchema extends EntityState<Comment> {
   isLoading?: boolean
   error?: string
-  data?: Comment[]
 }
