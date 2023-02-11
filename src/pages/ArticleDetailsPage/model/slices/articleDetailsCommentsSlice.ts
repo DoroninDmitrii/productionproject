31238522919631cf6a3cee836c19a6f86923b655
Dmitrii Doronin <doronin_dima@mail.ru>
2023-02-11 11:43:14 +0300

    51 fetchComments is ready

--- src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
+++ src/pages/ArticleDetailsPage/model/slices/articleDetailsCommentsSlice.ts
@@ -1,10 +1,12 @@
 import {
   createEntityAdapter,
-  createSlice
+  createSlice, 
+  PayloadAction
 } from '@reduxjs/toolkit'
 import { StateSchema } from 'app/providers/StoreProvider'
 import { Comment } from 'entities/Comment'
 import { ArticleDetailsCommentSchema } from '../types/ArticleDetailsCommentSchema'
+import { fetchCommentsByArticleId } from '../services/fetchCommentsByArticleId/fetchCommentsByArticleId'
 
 const commentsAdapter = createEntityAdapter<Comment>({
   selectId: (comment) => comment.id
@@ -22,8 +24,21 @@ const articleDetailsCommentsSlice = createSlice({
     ids: [],
     entities: {}
   }),
-  reducers: {
-
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCommentsByArticleId.pending, (state, action) => {
+        state.error = undefined
+        state.isLoading = true
+      })
+      .addCase(fetchCommentsByArticleId.fulfilled, (state, action: PayloadAction<Comment[]>) => {
+        state.isLoading = false
+        commentsAdapter.setAll(state, action.payload)
+      })
+      .addCase(fetchCommentsByArticleId.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
   }
 })
 
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -5,15 +5,33 @@ import { ArticleDetails } from 'entities/Article'
 import { Text } from 'shared/ui/Text/Text'
 import { useParams } from 'react-router-dom'
 import { CommentList } from 'entities/Comment'
+import { ReducerList, DynamicModuleLoader } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader'
+import { articleDetailsCommnetsReducer, getArticleComments } from '../../model/slices/articleDetailsCommentsSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { getArticleCommentsIsLoading } from '../../model/selectors/comments'
+import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect'
+import { fetchCommentsByArticleId } from '../../model/services/fetchCommentsByArticleId/fetchCommentsByArticleId'
 import cls from './ArticleDetailsPage.module.scss'
 
 interface ArticleDetailsPageProps {
   className?: string
 }
 
+const reducers: ReducerList = {
+  articleDetailsComments: articleDetailsCommnetsReducer
+}
+
 export const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   const { t, i18n } = useTranslation('article-details')
   const { id } = useParams<{ id: string }>()
+  const dispatch = useDispatch()
+
+  const comments = useSelector(getArticleComments.selectAll)
+  const commentsIsLoading = useSelector(getArticleCommentsIsLoading)
+
+  useInitialEffect(() => {
+    dispatch(fetchCommentsByArticleId(id))
+  })
 
   if (!id) {
     return (
@@ -24,11 +42,13 @@ export const ArticleDetailsPage = ({ className }: ArticleDetailsPageProps) => {
   }
 
   return (
-      <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
-          <ArticleDetails id={id} />
-          <Text className={cls.commentTitle} title={t('Comments')}/>
-          <CommentList isLoading={true} comments={[]} />
-      </div>
+      <DynamicModuleLoader reducers={reducers} removeAfterUnmount>
+          <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+              <ArticleDetails id={id} />
+              <Text className={cls.commentTitle} title={t('Comments')}/>
+              <CommentList isLoading={commentsIsLoading} comments={comments} />
+          </div>
+      </DynamicModuleLoader>
   )
 }
 
