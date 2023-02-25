45b3a2e25cd11b098bfaf98808e536d0e515c1cc
Dmitrii Doronin <doronin_dima@mail.ru>
2023-02-25 12:47:12 +0300

    54 add big size in article

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -6,8 +6,9 @@ import { Text } from 'shared/ui/Text/Text'
 import { Icon } from 'shared/ui/Icon/Icon'
 import { Card } from 'shared/ui/Card/Card'
 import EyeIcon from 'shared/assets/icons/eye-20-20.svg'
-import cls from './ArticleListItem.module.scss'
 import { Avatar } from 'shared/ui/Avatar/Avatar'
+import { Button, ButtonTheme } from 'shared/ui/Button/Button'
+import cls from './ArticleListItem.module.scss'
 
 interface ArticleListItemProps {
   className?: string
@@ -17,7 +18,15 @@ interface ArticleListItemProps {
 
 export const ArticleListItem = memo((props: ArticleListItemProps) => {
   const { className, article, view } = props
-  const { t, i18n } = useTranslation()
+  const { t, i18n } = useTranslation('article')
+
+  const types = <Text text={article.type.join(', ')} className={cls.types} />
+  const views = (
+      <>
+          <Text text={String(article.views)} className={cls.views} />
+          <Icon Svg={EyeIcon}/>
+      </>
+  )
 
   if (view === ArticleView.BIG) {
     return (
@@ -27,6 +36,15 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
                     <Avatar size={30} src={article.user.avatar} />
                     <Text text={article.user.username} className={cls.username} />
                     <Text text={article.createdAt} className={cls.date} />
+                </div>
+                <Text title={article.title} className={cls.title} />
+                {types}
+                <img src={article.img} className={cls.img} alt={article.title} />
+                <div className={cls.footer}>
+                    <Button theme={ButtonTheme.OUTLINE}>
+                        {t('Read more...')}
+                    </Button>
+                    {views}
                 </div>
             </Card>
         </div>
@@ -41,9 +59,8 @@ export const ArticleListItem = memo((props: ArticleListItemProps) => {
                   <Text text={article.createdAt} className={cls.date} />
               </div>
               <div className={cls.infoWrapper}>
-                  <Text text={article.type.join(', ')} className={cls.types} />
-                  <Text text={String(article.views)} className={cls.views} />
-                  <Icon Svg={EyeIcon}/>
+                  {types}
+                  {views}
               </div>
               <Text text={article.title} className={cls.title} />
           </Card>
