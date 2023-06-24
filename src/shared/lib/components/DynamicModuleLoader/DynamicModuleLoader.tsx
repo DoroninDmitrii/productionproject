1302de4970efac1726a39de107b15d476fae2dee
Dmitrii Doronin <doronin_dima@mail.ru>
2023-06-24 11:07:20 +0300

    101 reducelist fix

--- src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -1,11 +1,11 @@
 import { FC, ReactNode, useEffect } from 'react'
 import { useDispatch, useStore } from 'react-redux'
-import { ReduxStoreWithManager, StateSchemaKey } from '@/app/providers/StoreProvider'
+import { ReduxStoreWithManager, StateSchema, StateSchemaKey } from '@/app/providers/StoreProvider'
 import { Reducer } from '@reduxjs/toolkit'
 
 // for many reducers
 export type ReducerList = {
-  [name in StateSchemaKey]?: Reducer
+  [name in StateSchemaKey]?: Reducer<NonNullable<StateSchema[name]>>
 }
 
 type ReducerListEntry = [StateSchemaKey, Reducer]
