24810fffebd835a449f1719e496b0f895743a491
Dmitrii Doronin <doronin_dima@mail.ru>
2023-10-22 17:30:54 +0300

    148 fix bug with skeleton and rating

--- src/entities/Article/model/slice/articleDetailsSlice.ts
+++ src/entities/Article/model/slice/articleDetailsSlice.ts
@@ -4,33 +4,33 @@ import { ArticleDetailsSchema } from '../types/articleDetailsSchema';
 import { fetchArticlebyId } from '../services/fetchArticleById/fetchArticleById';
 
 const initialState: ArticleDetailsSchema = {
-  isLoading: false,
-  error: undefined,
-  data: undefined,
+    isLoading: false,
+    error: undefined,
+    data: undefined,
 };
 
 export const articleDetailsSlice = createSlice({
-  name: 'articleDetails',
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchArticlebyId.pending, (state, action) => {
-        state.error = undefined;
-        state.isLoading = true;
-      })
-      .addCase(
-        fetchArticlebyId.fulfilled,
-        (state, action: PayloadAction<Article>) => {
-          state.isLoading = false;
-          state.data = action.payload;
-        },
-      )
-      .addCase(fetchArticlebyId.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.payload;
-      });
-  },
+    name: 'articleDetails',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchArticlebyId.pending, (state, action) => {
+                state.error = undefined;
+                state.isLoading = true;
+            })
+            .addCase(
+                fetchArticlebyId.fulfilled,
+                (state, action: PayloadAction<Article>) => {
+                    state.isLoading = true;
+                    state.data = action.payload;
+                },
+            )
+            .addCase(fetchArticlebyId.rejected, (state, action) => {
+                state.isLoading = false;
+                state.error = action.payload;
+            });
+    },
 });
 
 export const { actions: articleDetailsAction } = articleDetailsSlice;
